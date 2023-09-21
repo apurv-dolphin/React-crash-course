4236c60829b50c25ae9d3fb2fedb08ff8d093842
apurv-dolphin <apurv.dolphinwebsolution@gmail.com>
2023-09-21 19:27:04 +0530

    remover unwanted css.

--- src/pages/_layouts/Home/Navbar.js
+++ src/pages/_layouts/Home/Navbar.js
@@ -37,13 +37,6 @@ export const Navbar = () => {
                 Star
               </a>
             </div>
-            <div
-              className="g-ytsubscribe ml-3"
-              data-channelid="UCdItDI6oTgPW7l9WOJI7ItA"
-              data-layout="default"
-              data-count="default"
-              style={{ height: "35px" }}
-            ></div>
           </div>
         </div>
 
